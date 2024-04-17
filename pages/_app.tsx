b7a507d19e3c3f8327f74560f04bfd7b532395e3
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2024-04-17 11:56:10 -0700

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,14 @@ import '@/styles/Noise.css'
 import '@/styles/Duet.css'
 import type { AppProps } from 'next/app'
 
+import { Analytics } from "@vercel/analytics/react"
+
 export default function App({ Component, pageProps }: AppProps) {
   useVHOverride()
   return (
     <WindowsProvider>
       <Component {...pageProps} />
+      <Analytics />
     </WindowsProvider>
   )
 }
