ad63e68576dba5ee74abf4afab16c9faa45c6e0b
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2024-06-18 15:45:51 -0700

    text copy

--- components/windows/InTheBeginning.tsx
+++ components/windows/InTheBeginning.tsx
@@ -41,7 +41,8 @@ const ITBWindow: FC = () => {
 
 
   const visit = () => {
-    typeof window !== "undefined" && window.open("https://www.tensor.trade/trade/its_just_noise", "_blank")
+    typeof window !== "undefined" &&
+      window.open("https://gamma.io/ordinals/collections/itb", "_blank");
   }
 
   const useSeed = (s: string) => {
@@ -65,42 +66,81 @@ const ITBWindow: FC = () => {
       label: "About",
       function: () => setActiveContent(prev => (prev + 1) % contentOptions.length)
     },
-    // {
-    //   label: "Trade",
-    //   function: visit
-    // },
+    {
+      label: "Mint",
+      function: visit
+    },
   ]
 
 
   return (
-    <Window windowKey={WINDOWS.ITB} initSize={initSize} initPosition={initPos} menu={menu} wrapperClassName="bg-amber-50">
+    <Window
+      windowKey={WINDOWS.ITB}
+      initSize={initSize}
+      initPosition={initPos}
+      menu={menu}
+      wrapperClassName="bg-amber-50"
+    >
       {/* {content} */}
-      <div className={clsx("w-full h-full duration-200", activeContent === 0 ? "opacity-100" : "opacity-0")}>
-        <InTheBeginning className={sketchCounter} menuOpen={seedOpenRef} seed={seed} isActive={isActiveRef} />
+      <div
+        className={clsx(
+          "w-full h-full duration-200",
+          activeContent === 0 ? "opacity-100" : "opacity-0"
+        )}
+      >
+        <InTheBeginning
+          className={sketchCounter}
+          menuOpen={seedOpenRef}
+          seed={seed}
+          isActive={isActiveRef}
+        />
       </div>
 
-      <div className={clsx("p-4 absolute top-0 left-0 h-full w-full overflow-auto", activeContent === 1 ? "block" : "hidden")}>
-        {/* <p>in the beginning was God</p>
-        <p>in the beginning were the Laws of Nature</p>
-        <p>from nothing came chaos</p>
-        <p>from chaos came beauty</p>
-        <p>into beauty we were born</p> */}
-
-
-{/* 
-        <br/> */}
+      <div
+        className={clsx(
+          "p-4 absolute top-0 left-0 h-full w-full overflow-auto",
+          activeContent === 1 ? "block" : "hidden"
+        )}
+      >
+        <p>
+          "in the beginning" is interactive generative art that explores the
+          metaphysical conflict of beauty and chaos.
+        </p>
+        <p>
+          It's a simulation of primordial soup with imposed order and simple
+          environmental rules.
+        </p>
+        <br />
+        <p>From this arises two conflicting organic elements.</p>
+        <p>
+          Each fighting for space, each spontaneously arising out of the other,
+          each able to be molded by the viewer.
+        </p>
+        <br />
+        <p>
+          Both are finely tuned to balance each other and maintain the pieces
+          form.
+        </p>
+        <p>
+          This form constantly shifts and grows around its initial momentum as
+          well as the viewers modifications.
+        </p>
+        <br />
         <ul className="list-disc list-inside">
           <li>drag the mouse to introduce new organic material</li>
           <li>Hold "e" and drag the mouse to erase organic material</li>
           <li>Press "k" to enter kill mode</li>
         </ul>
         <br />
-        <button className="classic-button px-2" onClick={() => setActiveContent(0)}>
+        <button
+          className="classic-button px-2"
+          onClick={() => setActiveContent(0)}
+        >
           Go back
         </button>
       </div>
     </Window>
-  )
+  );
 }
 
 export default ITBWindow
