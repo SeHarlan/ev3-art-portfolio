61a3e695b62a9e901d3e35188fd6516c27775206
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2024-06-18 15:11:18 -0700

    re

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,15 +8,15 @@ export default function Document() {
           rel="preconnect"
           href="https://fonts.googleapis.com"
         />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="anonymous"
-          />
-            <link
-              href="https://fonts.googleapis.com/css2?family=Kode+Mono:wght@400..700&family=VT323&display=swap"
-              rel="stylesheet"
-            />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kode+Mono:wght@400..700&family=VT323&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
